3171617b2dfa547d9295531fcdff8f51a9069e7c
仇剑昆 <jiankun.qiu@postgrad.manchester.ac.uk>
2022-04-25 15:27:23 +0100

    back-end skeleton

---
+++ student-system/src/test/java/com/comp62542/studentsystem/StudentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp62542.studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
