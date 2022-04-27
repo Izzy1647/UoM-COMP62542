f7335beb809d2940fb377d2e2d0353620235b5dd
zzy <zhouhoushu@126.com>
2022-04-27 10:04:10 +0100

    adapt router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import SignIn from './Components/SignIn/SignIn'
 
 function App() {
   return (
-    <SignIn />
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
