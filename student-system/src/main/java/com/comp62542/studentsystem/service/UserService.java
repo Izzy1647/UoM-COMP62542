343aad3f898a010b319d53782b12ea85caaa78d6
gemarkq <jk.qiu.mark@gmail.com>
2022-04-29 18:03:36 +0100

    login service

--- student-system/src/main/java/com/comp62542/studentsystem/service/UserService.java
+++ student-system/src/main/java/com/comp62542/studentsystem/service/UserService.java
@@ -2,8 +2,13 @@ package com.comp62542.studentsystem.service;
 
 import com.comp62542.studentsystem.dao.UserMapper;
 import com.comp62542.studentsystem.entity.User;
+import com.comp62542.studentsystem.util.JWTUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -17,5 +22,28 @@ public class UserService {
 
     public User findUserById(int id) {
         return userMapper.selectById(id);
+    }
+
+    public Map<String, Object> login(String StudentId) {
+        Map<String, Object> map = new HashMap<>();
+
+        if(StringUtils.isBlank(StudentId)) {
+            map.put("studentIdMsg", "StudnetId can not be empty!");
+            return map;
+        }
+        User user = userMapper.selectByStudentId(StudentId);
+        if(user == null) {
+            map.put("userMsg", "StudentId Incorrect!");
+            return map;
+        }
+
+        // JWT token
+        Map<String, String> payload = new HashMap<>();
+        payload.put("StudentId", user.getStudentID());
+        payload.put("name", user.getName());
+        String token = JWTUtils.getToken(payload);
+        map.put("token", token);
+
+        return map;
     }
 }
