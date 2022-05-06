bdbf15009607d987bdb76ac808a8ab52d09a48f7
zzy <zhouhoushu@126.com>
2022-05-06 13:55:46 +0100

    fetch activites and show'em

--- webapp/src/Components/Calendar/Calendar.tsx
+++ webapp/src/Components/Calendar/Calendar.tsx
@@ -4,11 +4,15 @@ import {
   WeekView,
   Appointments
 } from '@devexpress/dx-react-scheduler-material-ui'
+import moment from 'moment'
 // import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
+import Drawer from '@mui/material/Drawer'
+import Box from '@mui/material/Box'
 import { styled, alpha } from '@mui/material/styles'
 import { ViewState } from '@devexpress/dx-react-scheduler'
-import appointments from './mock-today-appointments'
+// import appointments from './mock-today-appointments'
+import { getActivities } from '../../api/calendar'
 
 const PREFIX = 'Demo'
 
@@ -82,7 +86,82 @@ const DayScaleCell = (props: any) => {
   return <StyledWeekViewDayScaleCell {...props} />
 }
 
+// activities data fetched from the server
+interface IActivityValue {
+  activityName: string
+  time: string
+  type: string
+}
+
+interface ICalendarValue {
+  startDate: Date
+  endDate: Date
+  title: string
+  location: string
+}
+
+const processActivityData = (data: IActivityValue): ICalendarValue => {
+  const { time, activityName } = data
+  console.log(time)
+
+  // !!time[0] to be modified after api update
+  const [weekday, startTime, endTime] = time[0].split('-')
+
+  const resStartTime = moment()
+    .startOf('week')
+    .add('days', Number(weekday))
+    .set('hour', Number(startTime.slice(0, 2)))
+    .set('minute', Number(startTime.slice(2)))
+
+  const resEndTime = moment()
+    .startOf('week')
+    .add('days', Number(weekday))
+    .set('hour', Number(endTime.slice(0, 2)))
+    .set('minute', Number(endTime.slice(2)))
+
+  console.log(resStartTime, resEndTime)
+
+  return {
+    startDate: resStartTime.toDate(),
+    endDate: resEndTime.toDate(),
+    title: activityName,
+    location: ''
+  }
+}
+
 const Calendar = () => {
+  const [drawerOpen, setDrawerOpen] = React.useState(false)
+  const [appointments, setAppointments] = React.useState([{}])
+
+  // send request
+  React.useEffect(() => {
+    async function fetchActivities() {
+      const res = await getActivities()
+
+      // data: IActivityValue[]
+      const activites = res.data as IActivityValue[]
+      const appointments = activites.map(item => processActivityData(item))
+      setAppointments(appointments)
+    }
+    fetchActivities()
+  }, [])
+
+  const onClickNewActivity = () => {
+    setDrawerOpen(true)
+  }
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return
+      }
+      setDrawerOpen(open)
+    }
+
   return (
     <>
       {/* @ts-ignore */}
@@ -97,7 +176,22 @@ const Calendar = () => {
         <Appointments />
       </Scheduler>
 
-      <Button style={{ margin: '20px auto' }} variant="contained">
+      <Drawer anchor={'right'} open={drawerOpen} onClose={toggleDrawer(false)}>
+        <Box
+          sx={{ width: 480 }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          13456
+        </Box>
+      </Drawer>
+
+      <Button
+        onClick={onClickNewActivity}
+        style={{ margin: '20px auto' }}
+        variant="contained"
+      >
         NEW ACTIVITY
       </Button>
     </>
